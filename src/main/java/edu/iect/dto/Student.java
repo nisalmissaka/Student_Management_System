b4aeb25ec6f64dc0ad036ebbaecfe5619af4bbe3
nisalmissaka <wishwakanisal@gmail.com>
2025-11-16 09:47:41 +0530

    Add Student DTO Class

---
+++ src/main/java/edu/iect/dto/Student.java
@@ -0,0 +1,25 @@
+package edu.iect.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String gender;
+    private int age;
+    private LocalDate dob;
+    private String grade;
+    private Double gpa;
+}
